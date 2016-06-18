4f23a31d667ffedab1c649b6b72f8462bcbb2354
Taff Gao <gaotongfei1995@gmail.com>
2016-06-18 21:55:58 +0800

    remove pg_test

--- src/pg_test.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(dbname="sf_development", user="gao", password="gaotongfei13")
-cur = conn.cursor()
-cur.execute("select id from scholars where id>=3000 and id <=7000")
-print(cur.fetchall())
