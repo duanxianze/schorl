9ab6167194ecefaa1b20f675ef5cf617a71edd90
tonylu <965606089@qq.com>
2016-06-21 16:48:44 +0800

    update task.py
    for solving complict

--- src/task.py
+++ src/task.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 '''
-    query.py 由学者姓名列表出发，做爬取分析，是本爬虫任务核心
+    task.py 由学者姓名列表出发，做爬取分析，是本爬虫任务核心
 '''
 import psycopg2
 import re
@@ -9,19 +9,30 @@ from request_with_proxy import request_with_proxy
 from math import ceil
 from parse_html import ParseHTML
 
-'''连接数据库'''
+'''设置数据库'''
 DB_NAME = "sf_development"
 USER = "gao"
 PASSWORD = "gaotongfei13"
+'''连接数据库'''
 conn = psycopg2.connect("dbname={0} user={1} password={2}".format(DB_NAME, USER, PASSWORD))
-conn.autocommit = True
+conn.autocommit = True  #设置数据库自动提交
 cur = conn.cursor()
 
-class QueryDB:
+class ScholarSearch:
     def __init__(self,name):
+        '''
+            传入参数：
+                name:       从学者表中检索出的学者名字元组，包含first_name, middle_name, last_name
+            包含属性：
+                self.name:        学者姓名元组
+                self.full_name :  学者完整姓名，将上方姓名元组整合得到
+                self.start_url :  搜索学者姓名，其结果的起始url
+        '''
         self.name = name
-        self.url = []
         if self.name[1].strip()=='':
+            '''
+                对于full_name的运算，若middle_name不存在，需要区分对待
+            '''
             self.full_name = self.name[0].strip() + ' ' + self.name[2].strip()
             self.start_url = 'https://scholar.google.com/scholar?start=0&q='+self.full_name+'&hl=en&as_sdt=0,5'
         else:
@@ -30,29 +41,36 @@ class QueryDB:
 
 
     def page(self):
-        '''得到该学者搜索结果的页数'''
+        '''
+            访问通过start_url，解析页面元素，得到文章数量
+            算出该学者搜索结果的页数，作为返回值
+        '''
         p = 10
         r = request_with_proxy(self.start_url)
         if r:
+            '''如果有返回结果'''
             soup = BeautifulSoup(r.text, "lxml")
             about = soup.select("#gs_ab_md")
             if about:
                 result = about[0].text
                 result_num = re.findall("About\s+([\w\,]+)\s+results", result)
                 if result_num:
+                    '''找到文章数，算出总页数'''
                     result_num = result_num[0].replace(",", "")
                     p = int(ceil(int(result_num)/float(10)))#ceil取整
         return p
 
 
     def page_urls(self):
-        '''得到包含该学者搜索结果的每页url的集合'''
+        '''
+            得到搜索结果页数，
+            并生成每页url的集合，作为返回值
+        '''
         pages = self.page()
         if pages>10:
             pages = 10
         '''10页以后意义不大，故大于10页取10页爬取'''
         urls = ['https://scholar.google.com/scholar?start={0}&q={1}&hl=en&as_sdt=0,5'.format(p*10-10, self.full_name) for p in range(1, pages+1)]
-
         return urls
 
 
@@ -64,15 +82,16 @@ names =  [n for n in cur.fetchall()]
 '''对于每一个学者'''
 for name in names:
     id = name[0]
-    query = QueryDB(name[1:])
+    scholar = ScholarSearch(name[1:])
     print(id)
     print(name)
     '''从实例化类中得到关于该学者的搜索结果的所有页面url'''
-    page_urls = query.page_urls()
+    page_urls = scholar.page_urls()
     for p in page_urls:
-        '''对于每一页，都交给ParseHTML模型解析'''
+        '''对于每一页'''
         print("for p in page")
         print(p)
+        '''都交给解析模型'''
         parse_html = ParseHTML(url=p)
         print(parse_html)
         print(parse_html.sections())
@@ -102,5 +121,6 @@ for name in names:
     '''更新数据库中学者的记录is_added = 1，表示已经爬取过他的文章集合'''
     cur.execute("update scholars set is_added = 1 where id = (%s)", (id,))
 
+'''与数据库连接断开'''
 cur.close()
 conn.close()
