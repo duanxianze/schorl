56ba9d9baace6b682bb9d724737e7a8bfc46e6cd
lyn716 <tonylu7116>
2016-08-01 16:34:26 +0800

    dump in dev2

--- src/auto_dump.py
+++ src/auto_dump.py
@@ -2,14 +2,14 @@ import os,time
 
 def auto_dump(date_str):
 	dump_file_name = date_str + '.sql'
-	os.system('pg_dump -U postgres -f '+ dump_file_name + ' sf_development')
+	os.system('pg_dump -U gao -f ~/backups/'+ dump_file_name + ' sf_development')
 
 
 if __name__ == '__main__':
+    '''
 	auto_dump('2015_5_23')
 	'''
 	while True:
 		date_str = time.strftime("%Y_%m_%d",time.localtime(time.time()))
 		auto_dump(date_str)
 		time.sleep(60*60*24)
-	'''
