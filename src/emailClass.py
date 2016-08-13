09584b944790769c8522cc8487382bb668343d0d
lyn716 <tonylu716>
2016-08-13 13:44:43 +0800

    add mail remind func for spider stop accidently

---
+++ src/emailClass.py
@@ -0,0 +1,91 @@
+#coding:utf-8
+"""
+@file:      emailClass.py
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.3
+@editor:    PyCharm
+@create:    2016-8-12 14:30
+@description:
+            smtp发送邮件，对email类做一个简单的封装
+"""
+
+import smtplib
+from email.mime.text import MIMEText  
+from email.header import Header
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+class Email:
+    def __init__(
+            self,sender,receiver,subject,content,
+            subtype='plain',img_src=None
+    ):
+        self.msg = MIMEMultipart('mixed')
+        msgText = MIMEText(content,_subtype=subtype,_charset='utf-8')
+        self.msg.attach(msgText)
+        if img_src:
+            fp = open(img_src,'rb')
+            msgImage = MIMEImage(fp.read())
+            msgImage.add_header('Content-ID','<meinv_image.png>')
+            self.msg.attach(msgImage)
+            fp.close()
+        self.msg['Subject'] = Header(subject, 'utf-8')
+        self.msg['From'] = sender
+        self.msg['To'] = receiver
+        self.sender = sender
+        self.receiver = receiver
+        self.smtp = smtplib.SMTP()
+
+
+    def conn_server(self,host,port):
+        #连接服务器,并启动tls服务
+        try:
+            self.smtp.connect(host,port)
+            self.smtp.starttls() 
+        except Exception as e:
+            print('conn_server():',e)
+
+
+    def login(self,username,password):
+        try:
+            self.smtp.login(username, password)
+            log_string = username+'登陆成功'+'\n'
+            print(log_string)#可以考虑写在日志里
+        except Exception as e:
+            print('login():',e)
+
+
+    def send(self):
+        try:
+            self.smtp.sendmail(self.sender, self.receiver, self.msg.as_string())
+            log_string = '邮件已投至'+self.receiver+'\n'
+            print(log_string)
+        except Exception as e:
+            print('send():',e)
+
+
+    def close(self):
+        self.smtp.close()
+
+
+
+if __name__=='__main__':
+    '''使用方法如下'''
+    emailAI = Email(
+        receiver='965606089@qq.com',
+        sender='luyangaini@vip.qq.com',
+        subject='this is test email',
+        content='fuck',
+    )
+    emailAI.conn_server(
+        host='smtp.qq.com',
+        port = 587
+    )
+    emailAI.login(
+        username='luyangaini@vip.qq.com',
+        password='xx'
+    )
+    emailAI.send()
+    emailAI.close()
---
+++ src/status_monitor.py
@@ -0,0 +1,78 @@
+#coding:utf-8
+"""
+@file:      status_monitor.py
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.3
+@editor:    PyCharm
+@create:    2016-8-12 14:30
+@description:
+            用于统计数据库articles表数据量,记录在log里,了解爬取速率,
+            若delta数据长时为零，则邮件通知管理员
+"""
+
+import psycopg2,time,csv
+from emailClass import Email
+
+COUNT_FROM_ARTICLES = 'select count(*) from articles'
+DB_NAME = "sf_development"
+USER = "gao"
+PASSWORD = "123123"
+conn = psycopg2.connect(
+    "dbname={0} user={1} password={2}".format(DB_NAME, USER, PASSWORD)
+)
+conn.autocommit = True
+cur = conn.cursor()
+
+
+def articles_amount(cur):
+    cur.execute(
+        'select count(*) from articles'
+    )
+    return cur.fetchall()[0][0]
+
+
+def send_mail(admin_address):
+    emailAI = Email(
+        receiver = admin_address,
+        sender   = 'luyangaini@vip.qq.com',
+        subject  = '爬虫停止工作了，上线看看吧',
+        content  = 'rt',
+    )
+    emailAI.conn_server(
+        host='smtp.qq.com',
+        port = 587
+    )
+    emailAI.login(
+        username='luyangaini@vip.qq.com',
+        password='ptuevbbulatcbcfh'
+    )
+    emailAI.send()
+    emailAI.close()
+
+
+
+if __name__=='__main__':
+    send_mail('965606089@qq.com')
+    tf = open('amount_log.txt','a+')
+    prev_amount = 0
+    initial = True
+    delta_zero_cot = 0
+    while(1):
+        amount = articles_amount(cur)
+        local_time = time.strftime("%Y-%m-%d-%H-%M-%S",time.localtime(time.time()))
+        delta = amount - prev_amount
+        if delta==0:
+            delta_zero_cot += 1
+        if delta_zero_cot==3:
+            send_mail('965606089@qq.com')
+            delta_zero_cot = 0
+        if initial:
+            delta = 0
+            initial = False
+        tf.write(str(amount)+','+local_time+','+str(delta)+'\n')
+        tf.close()
+        #update_csv()# operation not in server
+        tf = open('amount_log.txt','a+')
+        prev_amount = amount
+        time.sleep(10*60)#每十分钟统计一次
