54a7376d3310bd80d069370af42d855e58937f99
lyn716 <tonylu716>
2016-08-19 13:15:27 +0800

    shuffle db_item_list

--- src/download.py
+++ src/download.py
@@ -6,11 +6,10 @@
 @description:
             从db中检索出resource_link集,下载pdf
 """
-import os,time
+import os,time,random
 import requests
 requests.packages.urllib3.disable_warnings()
 from multiprocessing.dummy import Pool as ThreadPool
-from multiprocessing import Process
 import psycopg2
 
 '''设置数据库连接'''
@@ -26,6 +25,9 @@ DOWNLOAD_FOLDER = "./download"
 
 
 class PdfDownloader:
+    '''
+        pdf下载器类
+    '''
     def __init__(self,save_folder):
         #检查是否存在该文件夹，没有则创建
         if not os.path.exists(save_folder):
@@ -55,6 +57,7 @@ class PdfDownloader:
                 "update articles set is_downloaded = 1 where google_id = %s",
                 (google_id,)
             )
+            conn.commit()
         except Exception as e:
             print('download( google_id = "{}" ) ERROR:\n\t{}'.format(google_id,str(e)))
 
@@ -66,15 +69,13 @@ class PdfDownloader:
             )
             time.sleep(10)
 
-    def run(self):
-        print('length = ',len(self.statusMonitor.unfinished_items))
-        pool = ThreadPool(5)
-        pool.map(self.download, self.statusMonitor.unfinished_items)
+    def run(self,thread_counts=6):
+        print('unfinshed_item length: ',self.statusMonitor.counts_of_unfinished_db_item)
+        pool = ThreadPool(thread_counts)
+        unfinished_items = self.statusMonitor.unfinished_items
+        random.shuffle(unfinished_items)
+        pool.map(self.download, unfinished_items)
         #主循环，对于检索的结果列表中每个item都交给download函数执行
-        Process(
-            target=self.run_monitor(),
-            args=(1,)
-        ).start()
         pool.close()
         pool.join()
 
@@ -109,7 +110,7 @@ class StatusMonitor:
         cursor.execute(
             "select count(id) from articles where is_downloaded = 1"
         )
-        return cur.fetchall()
+        return int(cur.fetchall()[0][0])
 
     @property
     def counts_of_unfinished_db_item(self,extend_cursor=None):
@@ -119,9 +120,9 @@ class StatusMonitor:
         else:
             cursor = cur
         cursor.execute(
-            "select count(id) from articles where is_downloaded = 0"
+            "select count(*) from articles where is_downloaded = 0"
         )
-        return cur.fetchall()
+        return int(cursor.fetchall()[0][0])
 
     @property
     def counts_of_pdf_files(self,extend_folder=None):
@@ -134,5 +135,4 @@ class StatusMonitor:
 
 
 if __name__=='__main__':
-    PdfDownloader(DOWNLOAD_FOLDER).run()
-
+    PdfDownloader(DOWNLOAD_FOLDER).run(thread_counts=8)
