604be1737079a5ec9bf6f0d38523574b8a015b54
luyang <965606089@qq.com>
2016-08-26 14:45:52 +0800

    reconstruct bibtex

--- src/bibtex.py
+++ src/bibtex.py
@@ -1,68 +1,107 @@
 #coding:utf-8
-'''
-    bibtex.py 用于获取每篇文章的bibtex信息
-'''
+"""
+@file:      bibtex.py
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.3
+@editor:    PyCharm
+@create:    2016-08-26 14:08
+@description:
+            获取articles的bibtex
+"""
+
 from bs4 import BeautifulSoup
 from random import randint
 from request_with_proxy import request_with_proxy
 import psycopg2
 import time
-from fake_useragent import UserAgent
-
-ua = UserAgent()#实例化代理类
 
 random_port = lambda x, y: randint(x, y)#随机分配端口
 
-'''连接数据库'''
-conn = psycopg2.connect(dbname="sf_development", user="gao", password="gaotongfei13")
-conn.autocommit = True
+conn = psycopg2.connect(
+    dbname = "sf_development",
+    user = "gao",
+    password = "gaotongfei13"
+)
 cur = conn.cursor()
+conn.autocommit = True
 
-while True:
-    '''查询数据库，得到bibtex为空的项目元组'''
-    cur.execute("select id, google_id from articles where id > 314083 and bibtex is null")
-    result_sets = cur.fetchall()
-    print("{} tasks running".format(len(result_sets)))
-    for id, google_id in result_sets:
+def except_or_none(func):
+    def wrapper(*args, **kwargs):
         try:
-            '''对于一篇文章（item）'''
-            bibtex = None
-            url = 'https://scholar.google.com/scholar?q=info:{}:scholar.google.com/&output=cite&scirp=0&hl=en'.format(google_id)
+            return func(*args,**kwargs)
+        except Exception as e:
+            print('Bibtex:\n\tError in {}(): {}'.format(func.__name__,str(e)))
+            return None
+    return wrapper
 
+
+class Bibtex:
+    def __init__(self,soup,article_id=None):
+        self.soup = soup
+        if article_id:
+            self.article_id = article_id
+
+    @property
+    @except_or_none
+    def citi(self):
+        return self.soup.select("#gs_citi > a")
+
+    @property
+    @except_or_none
+    def url(self):
+        if self.citi:
+            return self.citi[0]['href']
+
+    @property
+    @except_or_none
+    def text(self):
+        print("get bibtex from {}".format(self.url))
+        bibtex_response = request_with_proxy(self.url)
+        print('bibtex site status code: {}'.format(bibtex_response.status_code))
+        if bibtex_response:
+            bibtex = bibtex_response.text
+            print('Bibtex:\n\t:Get new bibtex of the article:{}\n\t{}'.format(self.article_id,bibtex))
+            return bibtex
+        else:
+            return None
+
+    def save_to_db(self):
+        if self.text:
+            try:
+                cur.execute(
+                    "update articles set bibtex = %s where id = %s",
+                    (self.text, self.article_id)
+                )
+            except Exception as e:
+                print('Bibtex:Error:save_to_db():\n\t{}'.format(str(e)))
+
+
+class BibtexSpider:
+    def __init__(self):
+        pass
+
+    @property
+    def unfinished_items(self):
+        cur.execute("select id, google_id from articles where id > 314083 and bibtex is null")
+        return cur.fetchall()
+
+    def run(self):
+        for id, google_id in self.unfinished_items:
+            url = 'https://scholar.google.com/scholar?q=info:{}:scholar.google.com/&output=cite&scirp=0&hl=en'.format(google_id)
             print("id: {0} google_id: {1}".format(id, google_id))
             print("url: {}".format(url))
-            #print(request_with_proxy('https://api.ipify.org', use_ss=False).content)
-            '''爬虫访问文章url'''
-            #response = request_with_proxy(url, timeout=20, use_ss=False, sleep=1)
             response = request_with_proxy(url)
             if response.status_code == 200:
                 print("response 200")
-                '''若访问正常，对访问的返回结果注入BeautifulSoup模型'''
-                soup = BeautifulSoup(response.text, "lxml")
-                citi = soup.select("#gs_citi > a")
-                #with open(google_id+'.html', 'w') as f:
-                #f.write(response.text)
-                print(citi)
-                if citi:
-                    '''搜索节点，找到bibtex的url路径'''
-                    url = citi[0]['href']
-                    print("get bibtex from {}".format(url))
-                    '''访问该路径，得到bibtex'''
-                    bibtex_response = request_with_proxy(url)
-                    print('bibtex site status code: {}'.format(bibtex_response.status_code))
-                    if bibtex_response:
-                        bibtex = bibtex_response.text
-                        print(bibtex)
-                    else:
-                        print('no bibtex scraped')
-
+                Bibtex(
+                    soup = BeautifulSoup(response.text, "lxml"),
+                    article_id = id
+                ).save_to_db()
             else:
                 print(response.status_code)
+        time.sleep(3600)
 
-            '''对于得到的每一个文章的bibtex，更新注入数据库'''
-            cur.execute("update articles set bibtex = %s where id = %s", (bibtex, id))
-        except Exception as e:
-            print(e)
 
-    '''爬取延时'''
-    time.sleep(3600)
+if __name__=='__main__':
+    BibtexSpider().run()
