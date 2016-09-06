ffbdf6f0308e55e3c0a61fc94ce61149cb05271d
luyang <965606089@qq.com>
2016-09-06 20:45:48 +0800

    apply herit basic class in sons

---
+++ src/journal_pdf_url_generators/Elsevier_pdf_url_generator.py
@@ -0,0 +1,42 @@
+#coding:utf-8
+"""
+@file:      Elsevier_pdf_url_generator.py
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.3
+@editor:    PyCharm
+@create:    2016-09-06 13:18
+@description:
+            从db中获得elsevier杂志社的文章title集合，
+            生成pdf_url反馈给db
+"""
+from src.journal_parser.Elsevier_Parser import Elsevier_Parser
+from PdfUrlGenerator import *
+
+def get_elsevier_pdf_url_func(driver,unfinished_item):
+    return Elsevier_Parser(
+        article_page_url = unfinished_item[0],
+        driver = driver
+    ).pdf_url
+
+
+class Elsevier_pdf_url_generator(PdfUrlGenerator):
+    def __init__(self):
+        PdfUrlGenerator.__init__(self)
+
+    def get_unfinished_items(self,limit=10000):
+        #从db中检索出出版社为Elsevier的article title集
+        return self._get_unfinished_items(
+            query_sql = "select link,google_id from articles where resource_link is null \
+                  and journal_temp_info like '%Elsevier%' limit {}".format(limit)
+        )
+
+    def generate(self,unfinished_item):
+        return self._generate(unfinished_item,
+            google_id_index = 1,
+            get_pdf_url_func = get_elsevier_pdf_url_func
+        )
+
+
+if __name__=='__main__':
+    Elsevier_pdf_url_generator().run(thread_counts=8,visual=True)
--- src/journal_pdf_url_generators/IEEE_pdf_url_generator.py
+++ src/journal_pdf_url_generators/IEEE_pdf_url_generator.py
@@ -10,31 +10,12 @@
             专门为IEEE出版社的pdf下载模块
 """
 
-import os,psycopg2,random
-from multiprocessing.dummy import Pool as ThreadPool
+from PdfUrlGenerator import *
 from src.journal_parser.IEEE_Parser import IEEE_HTML_Parser,Article,get_pdf_link
-from src.crawl_tools.DriversPool import DriversPool
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
-if os.name is 'nt':
-    conn = psycopg2.connect(
-        host = '45.32.131.53',
-        port = 5432,
-        dbname = "sf_development",
-        user = "gao",
-        password = "gaotongfei13"
-    )
-else:
-    conn = psycopg2.connect(
-        dbname = "sf_development",
-        user = "gao",
-        password = "gaotongfei13"
-    )
-cur = conn.cursor()
-conn.autocommit = True
 
 class IEEE_Search_Model:
     def __init__(self,title,google_id,driver):
@@ -72,62 +53,34 @@ class IEEE_Search_Model:
             return get_pdf_link(pdf_page_url,self.driver)
 
 
-class IEEE_pdf_url_generator:
+def get_ieee_pdf_url_func(driver,unfinished_item):
+    pdf_temp_url = unfinished_item[2]
+    if pdf_temp_url:
+        return get_pdf_link(pdf_temp_url,driver)
+    else:
+        return IEEE_Search_Model(
+            title = unfinished_item[0],
+            google_id = unfinished_item[1],
+        ).get_pdf_url()
+
+class IEEE_pdf_url_generator(PdfUrlGenerator):
     def __init__(self):
-        self.drivers_pool = []
+        PdfUrlGenerator.__init__(self)
 
     def get_unfinished_items(self,limit=10000):
         #从db中检索出出版社为IEEE的article title集
-        cur.execute(
-            "select title,google_id,pdf_temp_url from articles where resource_link is null and journal_temp_info like '%ieee%' limit {}".format(limit)
+        return self._get_unfinished_items(
+            query_sql = "select title,google_id,pdf_temp_url from articles where resource_link is null\
+                    and journal_temp_info like '%ieee%' limit {}".format(limit)
         )
-        return cur.fetchall()
 
     def generate(self,unfinished_item):
         #从title出发，反馈pdf_url给db
-        google_id = unfinished_item[1]
-        pdf_temp_url = unfinished_item[2]
-        print('IEEE_PDF_URL_Generator:\n\tGot task of {}'.format(google_id))
-        #向pool索取一个空闲的driver对象
-        driverObj = self.drivers_pool.get_one_free_driver(wait=True)
-        driverObj.status = 'busy'
-        if pdf_temp_url:
-            pdf_url = get_pdf_link(pdf_temp_url,driverObj.engine)
-        else:
-            pdf_url = IEEE_Search_Model(
-                title = unfinished_item[0],
-                google_id = google_id,
-                driver = driverObj.engine
-            ).get_pdf_url()
-        driverObj.status = 'free'
-        if not pdf_url:
-            print('IEEE_PDF_URL_Generator:\n\tFail to get pdf_url of {}'.format(google_id))
-            return
-        print('IEEE_Article_Parser:\n\tGot pdf_url:{}'.format(pdf_url))
-        self.mark_db(pdf_url,google_id)
-
-    def mark_db(self,pdf_url,google_id):
-        try:
-            cur.execute(
-                "update articles set resource_link = %s,resource_type = 'PDF' where google_id = %s",
-                (pdf_url,google_id)
-            )
-            print('Database:\n\tupdate pdf_url of {} ok '.format(google_id))
-        except Exception as e:
-            print('Database:\n\tMark Error:{}'.format(str(e)))
-
-    def run(self,thread_counts=16,visual=True):
-        task_pool = ThreadPool(thread_counts)
-        self.drivers_pool = DriversPool(
-            size = thread_counts,
-            visual = visual,
-            launch_with_thread_pool = task_pool
+        return self._generate(unfinished_item,
+            google_id_index = 1,
+            get_pdf_url_func = get_ieee_pdf_url_func
         )
-        while(1):
-            result = task_pool.map(self.generate,self.get_unfinished_items(1000))
-            print(result)
-            task_pool.close()
-            task_pool.join()
+
 
 
 if __name__=='__main__':
@@ -137,4 +90,4 @@ if __name__=='__main__':
         print('killing {} ...'.format(proc.pid))
         os.kill(proc.pid,9)
 
-    IEEE_pdf_url_generator().run(thread_counts=16,visual=False)
+    IEEE_pdf_url_generator().run(thread_counts=8,visual=False)
