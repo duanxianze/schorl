42f94cf4eed30a6e9218e5f5b50ab2285500e887
lyn716 <tonylu716@gmail.com>
2016-09-15 22:49:47 +0800

    update

--- src/Journals_Task/got_journals_info.py
+++ src/Journals_Task/got_journals_info.py
@@ -14,52 +14,73 @@ from src.journal_parser.SJR_Parser import *
 from src.crawl_tools.DriversPool import DriversPool
 from multiprocessing.dummy import Pool as ThreadPool
 from src.db_config import new_db_cursor
-cur = new_db_cursor()
 
 
 class JournalInfoGenerator:
-    def __init__(self):
-        drivers_cot = 8
-        self.thread_pool = ThreadPool(drivers_cot)
-        self.drivers_pool = DriversPool(
-            visual=False,    size=drivers_cot,
-            launch_with_thread_pool=self.thread_pool,
-        )
+    def __init__(self,drivers_cot = 8):
+        if drivers_cot>0:
+            self.thread_pool = ThreadPool(drivers_cot)
+            self.drivers_pool = DriversPool(
+                visual=False,    size=drivers_cot,
+                launch_with_thread_pool=self.thread_pool,
+            )
 
     def get_db_category_ids(self):
+        cur = new_db_cursor()
         cur.execute(
             'select sjr_id,area_id from sjr_category'
         )
         return cur.fetchall()
 
+    def is_crawled_in_rank_page(self,category_id):
+        '''
+            判断某领域第一阶段是否爬过
+            搜索category和journal的关联表
+            若结果数多于10则说明已爬（多了也不需要）
+        '''
+        cur = new_db_cursor()
+        sql = 'select count(*) from journal_category where category_id = {}'.format(category_id)
+        #print(sql)
+        cur.execute(sql)
+        amount = cur.fetchall()[0][0]
+        print('JournalInfoGenerator:\n\tCategory_id:{},Amount:{}'.format(category_id,amount))
+        cur.close()
+        return amount>10
+
     def get_rank_journal_info(self,db_item):
+        #print(db_item)
+        category_id = db_item[0]
+        print('JournalInfoGenerator:\n\tGot task of {}'.format(category_id))
+        if self.is_crawled_in_rank_page(category_id):
+            print('JournalInfoGenerator:\n\t[Category crawled] {}'\
+                  .format(category_id))
+            return
         driverObj = self.drivers_pool.get_one_free_driver()
         driverObj.status = 'busy'
-        for sec in JournalRankPageParser(
-            category_id = db_item[0],
-            area_id = db_item[1],
-            driver = driverObj.engine
-        ).secs:
-            journal = RankJournal(sec)
-            journal.save_to_db(db_item[0])
-            #print(sec.text)
-            print('----------')
+        try:
+            for sec in JournalRankPageParser(
+                category_id = db_item[0],
+                area_id = db_item[1],
+                driver = driverObj.engine
+            ).secs:
+                RankJournal(sec).save_to_db(db_item[0])
+        except Exception as e:
+            print('JournalInfoGenerator:\n\t[Error] in get_rank_journal_info:{}'.format(str(e)))
         driverObj.status = 'free'
 
     def get_db_journal_ids(self):
-        cur.execute(
-            'select sjr_id from journal'
-        )
+        pass
 
     def get_detail_journal_info(self,db_item):
+        pass
 
     def run(self,mode):
         if mode==1:
             #mode 1 是在第一阶段,rank页journal没有全拿下来之前
             #主要是为了初始化生成条目
             self.thread_pool.map(
-                self.get_rank_journal_info,
-                self.get_db_category_ids()
+                func = self.get_rank_journal_info,
+                iterable = self.get_db_category_ids()
             )
         elif mode==2:
             #mode 2 第二阶段，进入详情页拿信息
@@ -70,5 +91,9 @@ class JournalInfoGenerator:
 
 
 if __name__=="__main__":
-    JournalInfoGenerator().run(mode=1)
+    from src.crawl_tools.WatchDog import close_procs_by_keyword
+    close_procs_by_keyword('phantom')
+    ge = JournalInfoGenerator(drivers_cot=8)
+    #ge.is_crawled_in_rank_page(category_id=3403)
+    ge.run(mode=1)
 
--- src/db_config.py
+++ src/db_config.py
@@ -1,6 +1,6 @@
 import os,psycopg2
 
-def new_db_cursor():
+def new_db_conn():
     if os.name is 'nt':
         conn = psycopg2.connect(
             host = '45.32.11.113',
@@ -16,4 +16,29 @@ def new_db_cursor():
             password = "tonylu716"
         )
     conn.autocommit = True
+    return conn
+
+
+CONN = new_db_conn()
+
+
+def new_db_cursor(new_conn=True):
+    if new_conn:
+        conn = new_db_conn()
+    else:
+        conn = CONN
     return conn.cursor()
+
+
+if __name__=="__main__":
+    import time
+    for i in range(10):
+
+        conn = new_db_conn()
+        cur = conn.cursor()
+        print(conn,cur)
+        #time.sleep(2)
+        #conn.close()
+        cur.close()
+        print(new_db_cur())
+        print('------------')
