fbbc75bf1ae3a34afef57bfafeb8ed82e8d6079e
luyang <965606089@qq.com>
2016-09-15 17:46:05 +0800

    new db_config to debug 'no result to fetch' when thread pool fetchall one cur

--- src/db_config.py
+++ src/db_config.py
@@ -1,18 +1,19 @@
 import os,psycopg2
 
-if os.name is 'nt':
-    conn = psycopg2.connect(
-        host = '45.32.11.113',
-        port = 5432,
-        dbname = "sf_development",
-        user = "lyn",
-        password = "tonylu716"
-    )
-else:
-    conn = psycopg2.connect(
-        dbname = "sf_development",
-        user = "lyn",
-        password = "tonylu716"
-    )
-cur = conn.cursor()
-conn.autocommit = True
+def new_db_cursor():
+    if os.name is 'nt':
+        conn = psycopg2.connect(
+            host = '45.32.11.113',
+            port = 5432,
+            dbname = "sf_development",
+            user = "lyn",
+            password = "tonylu716"
+        )
+    else:
+        conn = psycopg2.connect(
+            dbname = "sf_development",
+            user = "lyn",
+            password = "tonylu716"
+        )
+    conn.autocommit = True
+    return conn.cursor()
