dfe43e624a97c8bf71ce342c56d70bc23dccbb7e
luyang <965606089@qq.com>
2016-09-17 02:21:25 +0800

    rename

---
+++ src/Journals_Task/get_major_journals.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+"""
+@file:      get_major_journals.py
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.3
+@editor:    PyCharm
+@create:    2016-09-17 2:20
+@description:
+            ??
+"""
