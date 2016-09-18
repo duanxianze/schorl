ad2a38318dd614856a5d6f43a84246c2c7b3d530
lyn716 <tonylu716@gmail.com>
2016-09-18 21:14:01 +0800

    update

--- src/Journals_Task/ElsevierSpider.py
+++ src/Journals_Task/ElsevierSpider.py
@@ -10,7 +10,7 @@
             Elsevier针对某特定journal获取其古往今来的所有文章的爬虫
             上级模块有多线程分配，故此处用单线程写
 """
-from src.journal_parser.Elsevier_Parser import *
+from src.journal_parser.Elsevier_Parser import ElsevierAricle,ElsevierAllItemsPageParser
 from src.crawl_tools.request_with_proxy import request_with_random_ua
 
 class ElsevierSpider:
@@ -25,11 +25,13 @@ class ElsevierSpider:
         for volume_area_link in ElsevierAllItemsPageParser(
             html_source = request_with_random_ua(self.url).text
         ).volume_area_links:
+            #先分volume年份区间（十年）
             print(volume_area_link)
             volume_links = ElsevierAllItemsPageParser(
                 html_source = request_with_random_ua(volume_area_link).text
             ).volume_links
             volume_links.append(volume_area_link)
+            #得到该区间所有年份的page_url
             for volume_link in volume_links:
                 print(volume_link)
                 for sec in ElsevierAllItemsPageParser(
