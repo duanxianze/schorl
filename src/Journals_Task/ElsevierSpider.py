89348e7c52ff71790423928e09f09882263d10ba
lyn716 <tonylu716@gmail.com>
2016-09-18 19:50:30 +0800

    apply

--- src/Journals_Task/ElsevierSpider.py
+++ src/Journals_Task/ElsevierSpider.py
@@ -11,10 +11,46 @@
 """
 from src.db_config import new_db_cursor
 from src.journal_parser.Elsevier_Parser import *
+from src.crawl_tools.request_with_proxy import request_with_random_ua
+from src.crawl_tools.DriversPool import DriversPool
+import time,requests
 
 class ElsevierSpider:
     '''
         sample_url: http://www.sciencedirect.com/science/journal/15708268
     '''
-    def __init__(self):
+    def __init__(self,url):
         self.cur = new_db_cursor()
+        self.url = url
+
+    def run(self):
+        '''
+        pool = DriversPool(1)
+        driverObj = pool.get_one_free_driver()
+        driver = driverObj.engine
+        driver.get(self.url)
+        driverObj.status = 'free'
+        time.sleep(2)
+        ElsevierAllItemsPageParser(html_source=driver.page_source)
+        time.sleep(10)
+        '''
+        resp = request_with_random_ua(self.url)
+        parser = ElsevierAllItemsPageParser(
+            html_source = resp.text
+        )
+        for volume in parser.volumes:
+            print(volume)
+
+        print('ok')
+        time.sleep(555)
+
+        for sec in parser.secs:
+            article = ElsevierAricle(sec)
+            if article.type=='Original Research Article':
+                article.show_in_cmd()
+            print('----------')
+
+if __name__=="__main__":
+    ElsevierSpider(
+        url = 'http://www.sciencedirect.com/science/journal/15708268',
+    ).run()
