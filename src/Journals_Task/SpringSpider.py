117185a1d3f4267aa463a94f04a59784237f74d4
lyn716 <tonylu716@gmail.com>
2016-09-19 12:21:55 +0800

    update spring parser

--- src/Journals_Task/SpringSpider.py
+++ src/Journals_Task/SpringSpider.py
@@ -10,13 +10,40 @@
             ??
 """
 
-from src.db_config import new_db_cursor
-from src.journal_parser.Spring_Parser import *
+from src.journal_parser.Spring_Parser import SpringArticle,SpringParser
+from src.Journals_Task.JournalSpider import JournalSpider
+from src.crawl_tools.request_with_proxy import request_with_random_ua
+from bs4 import BeautifulSoup
 
-
-class SpringSpider:
+class SpringSpider(JournalSpider):
     '''
         sample_url: http://link.springer.com/search?sortOrder=newestFirst&facet-content-type=Article&facet-journal-id=778
     '''
-    def __init__(self):
-        self.cur = new_db_cursor()
+    def __init__(self,url,journal_id):
+        JournalSpider.__init__(self,journal_id)
+        self.url = url
+        self.handle_spring_url()
+
+    def handle_spring_url(self):
+        if 'www.springer.com' in self.url:
+            self.spring_journal_id = self.url.split('/')[-1]
+            self.url = 'http://link.springer.com/search?sortOrder=newestFirst&showAll=false&facet-content-type=Article&facet-journal-id='+self.spring_journal_id
+
+    def run(self):
+        parser = SpringParser(
+            html_source=request_with_random_ua(self.url).text
+        )
+        for page_num in range(1,parser.pages_amount+1):
+            page_url = 'http://link.springer.com/search/page/{}?facet-journal-id={}&showAll=false&facet-content-type=Article&sortOrder=newestFirst'\
+                            .format(page_num,self.spring_journal_id)
+            for sec in SpringParser(
+                html_source=request_with_random_ua(page_url).text
+            ).secs:
+                SpringArticle(sec).show_in_cmd()
+
+
+if __name__=="__main__":
+    SpringSpider(
+        url = 'http://www.springer.com/computer/database+management+%26+information+retrieval/journal/778',
+        journal_id=23792
+    ).run()
--- src/journal_parser/Spring_Parser.py
+++ src/journal_parser/Spring_Parser.py
@@ -9,6 +9,74 @@
 @description:
             ??
 """
+from bs4 import BeautifulSoup
+from src.crawl_tools.request_with_proxy import request_with_random_ua
 
 class SpringParser:
-    pass
+    def __init__(self,html_source=None,from_web=True):
+        if not from_web:
+            with open('Spring.html','rb') as f:
+                html_source = f.read()
+        self.soup = BeautifulSoup(html_source,'lxml')
+
+    @property
+    def pages_amount(self):
+        return int(self.soup.select_one('.number-of-pages').text)
+
+    @property
+    def secs(self):
+        return self.soup.select_one('#results-list').select('li')
+
+
+class SpringArticle:
+    def __init__(self,sec):
+        self.sec = sec
+
+    @property
+    def title(self):
+        return self.sec.select_one('.title').text
+
+    @property
+    def authors(self):
+        return list(map(lambda x:x.text,self.sec.select_one('.authors').select('a')))
+
+    @property
+    def link(self):
+        return 'http://link.springer.com' + self.sec.select_one('.title')['href']
+
+    @property
+    def abstract(self):
+        return self.sec.select_one('.snippet').text
+
+    @property
+    def year(self):
+        return int(self.sec.select_one('.year').text[1:-1])
+
+    @property
+    def pdf_url(self):
+        try:
+            return 'http://link.springer.com'+self.sec.select_one('.pdf-link')['href']
+        except:
+            return None
+
+    @property
+    def id_by_journal(self):
+        return
+
+    def show_in_cmd(self):
+        print('title:{}'.format(self.title))
+        print('authors:{}'.format(self.authors))
+        print('link:{}'.format(self.link))
+        print('abstract:{}'.format(self.abstract))
+        print('pdf_url:{}'.format(self.pdf_url))
+        print('year:{}'.format(self.year))
+        print('id_by_journal:{}'.format(self.id_by_journal))
+
+
+
+if __name__=="__main__":
+    for sec in SpringParser(from_web=False).secs:
+        SpringArticle(sec).show_in_cmd()
+        print('------------')
+
+
