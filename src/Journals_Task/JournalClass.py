96a06d9ab6aa9fb9a9dd2b69578d3834bdfe6ae3
luyang <965606089@qq.com>
2016-09-19 23:19:42 +0800

    add journal class in db

---
+++ src/Journals_Task/JournalClass.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+"""
+@file:      JournalClass.py
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.3
+@editor:    PyCharm
+@create:    2016-09-19 21:40
+@description:
+            ??
+"""
+
+class Journal:
+    def __init__(self):
+        self.sjr_id = None
+        self.name = None
+        self.country = None
+        self.site_source = None
+        self.area_relation_cot = None
+        self.category_relation_cot = None
+        self.issn = None
+        self.is_crawled_all_article = None
+        self.open_access = None
+        self.h_index = None
+        self.publisher = None
