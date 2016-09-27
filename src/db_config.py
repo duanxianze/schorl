1eb729e661869292785e2e4e14ec09bf522944f0
lyn716 <tonylu716@gmail.com>
2016-09-27 19:45:49 +0800

    近期以实验室主机为主数据库

--- src/db_config.py
+++ src/db_config.py
@@ -1,25 +1,25 @@
 from crawl_tools.DB_Connect_Pool import DB_Connect_Pool
 import os
 
-#linux机器为主数据库
+#windows机器为主数据库
 
 if os.name=='nt':
-    REMOTE_HOST = '45.32.11.113'
-    REMOTE_PORT = 5432
+    REMOTE_HOST = None
+    REMOTE_PORT = None
 else:
     REMOTE_HOST = None
     REMOTE_PORT = None
 
 
 DB_CONNS_POOL = DB_Connect_Pool(
-    size=20,
+    size = 32,
     dbname = "sf_development",
     user = "lyn",
     password = "tonylu716",
 )
 
 REMOTE_CONNS_POOL = DB_Connect_Pool(
-    size = 5,
+    size = 4,
     dbname = "sf_development",
     user = "lyn",
     password = "tonylu716",
