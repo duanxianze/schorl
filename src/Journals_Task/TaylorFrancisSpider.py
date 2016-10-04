7ca0caaf8356bb69162f61f6a0827c5ea5412d69
lyn716 <tonylu716@gmail.com>
2016-10-04 22:40:42 +0800

    为tf杂志社撰写好爬虫，测试下来屏蔽措施较屌

--- src/Journals_Task/TaylorFrancisSpider.py
+++ src/Journals_Task/TaylorFrancisSpider.py
@@ -17,29 +17,70 @@ for i in range(up_level_N):
     root_dir = os.path.normpath(os.path.join(root_dir, '..'))
 sys.path.append(root_dir)
 
-
 from journal_parser.TaylorFrancis_Parser import TaylorFrancisArticle,TaylorFrancisParser
 from Journals_Task.JournalSpider import JournalSpider
 from crawl_tools.request_with_proxy import request_with_random_ua
+import requests,re
+from bs4 import BeautifulSoup as BS
+
 
 class TaylorFrancisSpider(JournalSpider):
+    '''
+        sample url:http://www.tandfonline.com/toc/ghbi20/current
+    '''
     def __init__(self,JournalObj):
         JournalSpider.__init__(self,JournalObj)
         self.JournalObj = JournalObj
         self.url = JournalObj.site_source
-        self.handle_spring_url()
+        self.handle_taylor_url()
         self.generate_volume_links()
 
+    '''
+    #法一，到专门的volume_link集合页寻找，需要记录session区间，已被淘汰
     def handle_taylor_url(self):
+        journal_id = self.url.split('/')[-2]
+        self.url = 'http://www.tandfonline.com/loi/{}'.format(journal_id)
+        print(self.url)
+
+    def generate_volume_links(self):
+        if self.JournalObj.volume_links_got:
+            return
+        with requests.Session() as s:
+            for url in list(map(
+                lambda x:'http://www.tandfonline.com'+x['href'],
+                BS(requests.get(self.url).text,'lxml')\
+                        .find_all(class_='volume_link')
+            )):
+                resp = s.get(url)
+        self.volume_links = list(map(
+            lambda x:'http://www.tandfonline.com'+x['href'],
+            BS(resp.text,'lxml').find_all(
+                href = re.compile("nav=tocList")
+            )
+        ))
+    '''
+    def handle_taylor_url(self):
+        #先指current卷页面，volume_link信息每页都有
         pass
 
     def generate_volume_links(self):
         if self.JournalObj.volume_links_got:
             return
-        pass
+        self.volume_links = TaylorFrancisParser(
+            html_source=request_with_random_ua(self.url).text
+        ).volume_links
 
     def run(self):
         self._run(
             AllItemsPageParser = TaylorFrancisParser,
             JournalArticle = TaylorFrancisArticle
         )
+
+if __name__=="__main__":
+    from Journals_Task.JournalClass import Journal
+    JournalObj = Journal()
+    JournalObj.site_source = 'http://www.tandfonline.com/toc/ghbi20/current'
+    JournalObj.sjr_id = 123
+    spider = TaylorFrancisSpider(JournalObj)
+    print(spider.volume_links)
+
--- src/journal_parser/TaylorFrancis_Parser.py
+++ src/journal_parser/TaylorFrancis_Parser.py
@@ -17,12 +17,28 @@ for i in range(up_level_N):
     root_dir = os.path.normpath(os.path.join(root_dir, '..'))
 sys.path.append(root_dir)
 
+import re,requests
 from bs4 import BeautifulSoup
 from journal_parser.JournalArticle import JournalArticle
+from crawl_tools.decorators import except_pass,except_return_none
+ERN_METHOD = lambda func:except_return_none(func,'TaylorFrancisParser')
+EP_METHOD = lambda func:except_pass(func,'TaylorFrancisArticle')
+
+
+class TF_DetailPageParser:
+    '''
+        http://www.tandfonline.com/doi/abs/10.1080/08912968809386468
+    '''
+    def __init__(self,url):
+        self.soup = BeautifulSoup(
+            requests.get('http://www.tandfonline.com/doi/abs/10.1080/08912968809386468').text,
+            'lxml'
+           )
 
 class TaylorFrancisParser:
     '''
-        sample url:http://www.tandfonline.com/toc/fjps20/current
+
+        sample_url: http://www.tandfonline.com/toc/ghbi20/28/8?nav=tocList
     '''
     def __init__(self,html_source=None,from_web=True):
         if not from_web:
@@ -31,16 +47,67 @@ class TaylorFrancisParser:
         self.soup = BeautifulSoup(html_source,'lxml')
 
     @property
+    @ERN_METHOD
     def sections(self):
-        pass
+        return self.soup.select('.tocArticleEntry')
 
     @property
+    @ERN_METHOD
     def volume_links(self):
-        pass
-
+        return list(map(
+            lambda x:'http://www.tandfonline.com'+x['href'],
+            self.soup.find_all(href=re.compile("nav=tocList"))
+        ))
 
 class TaylorFrancisArticle(JournalArticle):
     def __init__(self,sec,JournalObj,volume_db_id):
         self.sec = sec
         JournalArticle.__init__(self,JournalObj,volume_db_id)
         self.generate_all_method()
+
+    @property
+    @ERN_METHOD
+    def access_get(self):
+        return self.sec.select_one('.access-icon')
+
+    @EP_METHOD
+    def generate_title(self):
+        self.title = self.sec.select_one('.art_title > a > span').text
+
+    @EP_METHOD
+    def generate_authors(self):
+        self.authors = list(map(
+            lambda x:x.text,
+            self.sec.select('.articleEntryAuthorsLinks > span > a')
+        ))
+
+    @EP_METHOD
+    def generate_link(self):
+        self.link = 'http://www.tandfonline.com'+self.sec.select_one('.art_title > a')['href']
+
+    @EP_METHOD
+    def generate_abstract(self):
+        pass
+
+    @EP_METHOD
+    def generate_year(self):
+        self.year = int(self.sec.select_one('.tocEPubDate').text.split(' ')[-1])
+
+    @EP_METHOD
+    def generate_pdf_temp_url(self):
+        if self.access_get:
+            self.pdf_temp_url = 'tf_open'
+        else:
+            self.pdf_temp_url = 'tf_close'
+
+    @EP_METHOD
+    def generate_id_by_journal(self):
+        self.id_by_journal = 'TF'+self.sec.select_one('.art_title > a')['href'].split('/')[-1]
+
+if __name__=="__main__":
+    from Journals_Task.JournalClass import Journal
+    JournalObj = Journal()
+    JournalObj.site_source = 'http://www.tandfonline.com/toc/ghbi20/current'
+    JournalObj.sjr_id = 123
+    for sec in TaylorFrancisParser(from_web=False).sections:
+        TaylorFrancisArticle(sec,JournalObj,2).show_in_cmd()
